94ec1c5a946cbf873d2c11dc00a86c23b8e68ca6
Edward Pol <edpol@p4bh.org>
2017-12-10 21:50:15 -0500

    Added Maps

--- laravel/resources/views/advisors/show.blade.php
+++ laravel/resources/views/advisors/show.blade.php
@@ -1,61 +1,42 @@
+<?php $tab="Show Advisor"; ?>
 @extends('layouts.master')
 
 @section('box1')
-    <header class="intro-header">
-    </header>
+<div class="container">
+    &nbsp;
+</div>
 @endsection
 
 @section('box2')
-<section class="content-section-a">
+<section class="content-section-b">
     <div class="container">
         <div class="row">
-            <div>
-                <hr class="section-heading-spacer">
+            <div class="col-lg-6" style="float:left;">
                 <div class="clearfix"></div>
 <?php // this one is only returning one record so ->body works ?>
-                <div style="float:left; padding-right:6px;">
-                    name: <br />
-                    phone: <br />
-                    email: <br />
-                    company: <br />
-                    address: <br />
-                    <br />
-                    minimum amount: <br />
-                    maximum amount: <br />
-                    minimum fee: <br />
-                    fee calculatuin: <br />
-                    lat/lng: <br />
-                    <br />
-                </div>
-
-                <div style="float:left;">
-                    {{ $advisor->name  }} <br />
-                    {{ $advisor->phone }} <br />
-                    {{ $advisor->email }} <br />
-
-                    @if(isset($advisor->url) && !empty($advisor->url))
-                        <a href="{{ App\Advisor::addScheme($advisor->url) }}" target="_blank">
-                    @endif
-                    {{ $advisor->company }} 
-                    @if(isset($advisor->url) && !empty($advisor->url))
-                        </a> 
-                    @endif
-                    <br />
-                    {{ $advisor->address1 }} 
-                    {{ $advisor->address2 }} <br />
-                    {{ $advisor->city }}, {{ $advisor->st }} {{ $advisor->zip }} <br />
-
-
-                    {{ number_format($advisor->minimum_amt, 0) }} <br />
-                    {{ number_format($advisor->maximum_amt, 0) }} <br />
-                    {{ number_format($advisor->minimum_fee, 0) }} <br />
-                    {{ $advisor->feeCalculation }} <br />
-                    {{ $advisor->lat }}   {{ $advisor->lng }} <br />
-                    <a href="{{ $advisor->brochure }}" target="_blank">Part 2 Brochure</a> <br />
-                </div>
-                <br clear="all" />
-                    desc: &nbsp; {{ $advisor->blurb }} 
+                @include('advisors.display')
             </div>
+
+<?php
+        $key[0] = "AIzaSyALzhEzkuqN7XpucdVcJUxR12p2X0W5LnE"; 
+        $key[1] = "AIzaSyCdltmUqKisvFuUxvU-Ljf7CmTAjV0GZqw";
+        $key[2] = "AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8";
+
+        $q = $advisor->address1 . " " . $advisor->address2 . " " . $advisor->city . " " . $advisor->st . " " . $advisor->zip . " Unites States";
+        $q = preg_replace('/\s+/', ' ',$q);
+        $q = str_replace(" ","+",$q);
+?>
+
+        <div class="col-lg-6">
+            <div id="mymap-display" style="height:100%; width:100%;max-width:100%;">
+                <iframe style="height:100%;width:100%;border:0;" frameborder="0" 
+                    src="https://www.google.com/maps/embed/v1/place?q=<?= $q ?>&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8">
+                </iframe>
+            </div>
+            <style>#mymap-display .text-marker{}.map-generator{max-width: 100%; max-height: 100%; background: none;</style>
+        </div>
+
+            <br clear="all" />
         </div>
     </div>
 </section>
