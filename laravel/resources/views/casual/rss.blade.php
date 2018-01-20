e7c910ddbd549a3f725fe6b527f544ac38d660d3
Edward Pol <edpol@p4bh.org>
2018-01-19 23:41:36 -0500

    Changed RSS Feed

--- laravel/resources/views/casual/rss.blade.php
+++ laravel/resources/views/casual/rss.blade.php
@@ -11,40 +11,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <h1>NASDAQ - Stocks</h1>
+                <h1>Financial Advisor - Online Articles</h1>
                 <br />
                 <div class="panel-body">
-                    <ul>
-<?php
-                        $rss = new DOMDocument();
-                        //    	$rss->load('http://wordpress.org/news/feed/');
-                        //      $rss->load("http://feeds2.feedburner.com/InvestingRss");
-                        $rss->load("http://articlefeeds.nasdaq.com/nasdaq/categories?category=Stocks");
-
-                        $feed = array();
-                        foreach ($rss->getElementsByTagName('item') as $node) {
-                            $item = array ( 
-                                'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-                                'desc'  => $node->getElementsByTagName('description')->item(0)->nodeValue,
-                                'link'  => $node->getElementsByTagName('link')->item(0)->nodeValue,
-                                'date'  => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
-                            );
-                            array_push($feed, $item);
-                        }
-                        $limit = 6;
-                        for($x=0;$x<$limit;$x++) {
-                            $article = $feed[$x];
-                            $title = str_replace(' & ', ' &amp; ', $article['title']);
-                            $link = $article['link'];
-                            $description = $article['desc'];
-                            $date = date('l F d, Y', strtotime($article['date']));
-                            echo "<li>\n";
-                            echo '<p><strong><a href="'.$link.'" title="'.$title.'">'.$title.'</a></strong><br />';
-                            echo '<small><em>Posted on '.$date.'</em></small></p>';
-                            echo '<p>'.$description.'</p>';
-                            echo "</li>\n";
-                        }
-?>                  </ul>
+                    <?= htmlspecialchars_decode($msg); ?>
                 </div>
             </div>
         </div>
