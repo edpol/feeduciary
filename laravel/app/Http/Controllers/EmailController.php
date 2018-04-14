6908d1cdd04b534a3b32ae164807694fba7cd334
Edward Pol <edward.g.pol@gmail.com>
2018-04-14 12:38:11 -0400

    Removed extra folders

--- laravel/app/Http/Controllers/EmailController.php
+++ laravel/app/Http/Controllers/EmailController.php
@@ -14,6 +14,16 @@ class EmailController extends Controller
 {
     public function contactUs(Request $request)
     {
+
+
+$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+$resp = $recaptcha->verify($gRecaptchaResponse, $remoteIp);
+if ($resp->isSuccess()) {
+    // verified!
+    // if Domain Name Validation turned off don't forget to check hostname field
+    // if($resp->getHostName() === $_SERVER['SERVER_NAME']) {  }
+
+
         $name    = $request->input('name');
         $email   = $request->input('email');
         $phone   = $request->input('phone');
@@ -30,6 +40,9 @@ class EmailController extends Controller
 
         \Mail::to($toEmail)->send(new ContactUs($data));
         return view('casual.thankYou', compact('data'));
+} else {
+    $errors = $resp->getErrorCodes();
+}
     }
 
     public function send(Request $request)
--- laravel/resources/views/layouts/master.blade.php
+++ laravel/resources/views/layouts/master.blade.php
@@ -7,9 +7,9 @@
 	<meta name="keywords" content="Feeduciary, fiduciary, fiduciaries, Fee, Cost, Annual, Advisor, Commission, Fee based, Fee only, Fiduciary Rule, Custodian, Leverage, Save, Time, Wealth, Financial, Consulting, Advice, Stocks, 401k, roth, IRA, savings, cheap, discount, independent, dealer, Broker, RIA, Registered, Investment, Mutual fund, ETF, CD, Savings, Growth, Value, Small cap, Large cap, Mid cap, Dollars, Annuity, REIT, Fixed, Variable, Interest Rate, Inflation, Retirement, Calculator, Planner, Financial, CFP, CFA, Plan, Save, Economy, Market, CMBS, MBS, Agency, Debt, Tax free, Sec, Finra, Broker dealer check, Taxable, Refinance, Compete, Facilitate, SEC Rule, Liquidity, Loan, Bank, Rate, Effective, Reform, Short Duration, Vanguard, Fidelity, Real estate, Loomis, Prudential, Invesco, Eaton Vance, Putnam, natixis, JP Morgan, MLP, Pipeline, Midstream, Upstream, Downstream, Floating rate, High yield, Low, Dividend, Income, Distribution, Credit, Risk, Capital gain, Long term, Short term, NAV, Technology, Discount, rule, Sensitive, Reoccurring, Alternative, Schwab, Fidelity" />
 	<title>Feeduciary <?php if (isset($tab)) { echo " | {$tab}"; } ?></title>
 	<!-- Bootstrap core CSS -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
 	<!-- Custom fonts for this template -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/font-awesome/css/font-awesome.min.css') }}" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}" />
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" />
 	<!-- Custom styles for this template -->
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/landing-page.css') }}" />
@@ -24,7 +24,7 @@
 
 		gtag('config', 'UA-112524987-1');
 	</script>
-
+	<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
 
