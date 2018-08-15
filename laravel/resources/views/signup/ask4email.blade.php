3535af0e46c8bb9b5d70aeb7fb3294b62c9313c0
Edward Pol <edpol@p4bh.org>
2018-08-15 18:04:06 -0400

    change tab order

--- laravel/resources/views/signup/ask4email.blade.php
+++ laravel/resources/views/signup/ask4email.blade.php
@@ -19,19 +19,20 @@ Enter your email address to access Feeduciary's free fee-based advisor search fe
                                 <div class="m-3">
                                     <div class="form-group my-3 my-md-2 row">
                                         <label class="col-md-2 pt-2" for="email">*Email:</label>
-                                        <input class="form-control col-md-9 col-12" type="email" id="email" name="email" value="{{ $email }}" autofocus />
+                                        <input class="form-control col-md-9 col-12" type="email" id="email" name="email" value="{{ $email }}" tabindex=1 autofocus />
                                     </div>
                                     <div class="form-group mb-2 mb-md-3 row">
                                         <label class="col-md-2 pt-2" for="name">Name:</label>
-                                        <input class="form-control col-md-9 col-12" type="text"  id="name"  name="name"  value="{{ $name }}" />
+                                        <input class="form-control col-md-9 col-12" type="text"  id="name"  name="name"  value="{{ $name }}" tabindex=2 />
                                     </div>
  @include('signup.debug')
                                     <div class="modal-footer row">
                                         @guest
-                                        <button type="submit" class="btn btn-info    mb-1 col-md-5 col-12 mr-0" formaction="{{ url('/register') }}">Advisors Register Here</button>
+                                        <button type="submit" tabindex=5 class="btn btn-info    mb-1 col-md-5 col-12 mr-0" formaction="{{ url('/register') }}">Advisors Register Here</button>
                                         @endguest
-                                        <button type="button" class="btn btn-default mb-1 col-md-2 col-12 mr-0" data-dismiss="modal">Close</button>
-                                        <button type="submit" class="btn btn-primary mb-1 col-md-2 col-12 mr-0">Save</button>
+                                        <button type="button" tabindex=4 class="btn btn-default mb-1 col-md-2 col-12 mr-0" data-dismiss="modal">Close</button>
+
+                                        <button type="submit" tabindex=3 class="btn btn-primary mb-1 col-md-2 col-12 mr-0">Save</button>
                                     </div>
                                 </div>
                             </div>
